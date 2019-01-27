ec4d134524e1db7c2893e5cfabef7e5fee5f957c
Jorge Diaz <jorgediaz@hotmail.com>
2019-01-27 18:44:46 -0300

    Postgresql funca

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181109131138) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "responsables", force: :cascade do |t|
     t.string "nombre"
     t.boolean "docente"
@@ -23,11 +26,12 @@ ActiveRecord::Schema.define(version: 20181109131138) do
   create_table "turnos", force: :cascade do |t|
     t.date "fecha"
     t.string "horario"
-    t.integer "responsable_id"
+    t.bigint "responsable_id"
     t.date "solicitado"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["responsable_id"], name: "index_turnos_on_responsable_id"
   end
 
+  add_foreign_key "turnos", "responsables"
 end
